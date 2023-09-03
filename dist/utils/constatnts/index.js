7e84fab005ac39b647a4b81856e45f69ab92a019
its-kayes <kayes.ek8@gmail.com>
2023-09-03 22:04:05 +0600

    build with new update

--- dist/utils/constatnts/index.js
+++ dist/utils/constatnts/index.js
@@ -5,12 +5,12 @@ var ENUM_USER_ROLE;
 (function (ENUM_USER_ROLE) {
     ENUM_USER_ROLE["ADMIN"] = "admin";
     ENUM_USER_ROLE["CUSTOMER"] = "customer";
-})(ENUM_USER_ROLE = exports.ENUM_USER_ROLE || (exports.ENUM_USER_ROLE = {}));
+})(ENUM_USER_ROLE || (exports.ENUM_USER_ROLE = ENUM_USER_ROLE = {}));
 // pagination props
 exports.paginationProps = ['page', 'size', 'sortBy', 'sortOrder'];
 // book search and filter
-exports.bookFilterableFields = ['searchTerm', 'title', 'author'];
-exports.bookSearchableFields = ['title', 'author', 'genre',];
+exports.bookFilterableFields = ['searchTerm', 'minPrice', 'maxPrice', 'title', 'author', 'categoryId'];
+exports.bookSearchableFields = ['title', 'author', 'genre'];
 exports.bookRelationalFields = ['categoryId'];
 exports.bookRelationalFieldsMapper = {
     categoryId: 'category',
